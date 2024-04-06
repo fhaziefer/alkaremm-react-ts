0b828b9df48e441a5fd6d1ed5aad39461484b12d
Yusuf Fadlulloh <fhaziefer99@gmail.com>
2024-04-07 04:57:19 +0700

    update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import Button from "./Components/Ui/Button";
-import CheckBox from "./Components/Ui/CheckBox";
-
 function App() {
 
   return (
     <div className="prose">
       <h1>Hai</h1>
     </div>
-  );
+  )
 }
 
 export default App;
