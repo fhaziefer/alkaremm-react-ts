b55fef61e33d862f3ffe7ac8cc318b4769577e80
Yusuf Fadlulloh <fhaziefer99@gmail.com>
2024-04-07 04:56:40 +0700

    create modal components

---
+++ src/Components/Ui/Modal.tsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+import cn from "classnames"
+import { useOnClickOutside } from "usehooks-ts";
+
+type CheckBoxProps = React.HTMLAttributes<HTMLInputElement> & {
+    label?: string;
+    body?: string;
+    open: boolean;
+    onClose(): void;
+}
+
+export default function Modal({ open, onClose, label, body }: CheckBoxProps) {
+    const ref = useRef(null);
+    useOnClickOutside(ref, () => {
+        onClose();
+    });
+    const modalClass = cn({
+        "modal modal-bottom sm:modal-middle": true,
+        "modal-open": open,
+    });
+    return (
+        <div>
+            <dialog className={modalClass}>
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">{label}</h3>
+                    <p className="py-4">{body}</p>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button onClick={() => onClose()}></button>
+                </form>
+            </dialog>
+        </div>
+    )
+}
