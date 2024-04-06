05ed067877772160478b4784c884b92bd7c3720c
Yusuf Fadlulloh <fhaziefer99@gmail.com>
2024-04-07 05:34:51 +0700

    create header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,13 +7,10 @@ const Header = () => {
                 <a className="btn btn-ghost text-xl rounded-full hover:text-white">Alkareem</a>
             </div>
             <div className="flex-none gap-2">
-                <div className="form-control">
-                <input type="text" placeholder="search.." className="input w-full max-w-xs" />
-                </div>
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img alt="Tailwind CSS Navbar component" src="http://localhost:300/images/avatar/male.jpg" />
+                        <div className="rounded-full">
+                            <img alt="User avatar" src="http://localhost:300/images/avatar/male.jpg" />
                         </div>
                     </div>
                     <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
