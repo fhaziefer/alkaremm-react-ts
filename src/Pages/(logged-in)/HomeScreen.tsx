4d0e7a99de19f2b959684229fd12dff3d8ee1151
Yusuf Fadlulloh <fhaziefer99@gmail.com>
2024-04-08 08:19:49 +0700

    axios get method

--- src/Pages/(logged-in)/HomeScreen.tsx
+++ src/Pages/(logged-in)/HomeScreen.tsx
@@ -1,21 +1,59 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import Button from '../../Components/Ui/Button'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../Components/Ui/Button';
+import axios from 'axios';
+import { TypeUsers } from '../../Types/Alkareem/GetAllUserRes';
 
-type Props = {}
+type Props = {};
 
 const HomeScreen = (props: Props) => {
-  const navigate = useNavigate()
+  const [users, setUsers] = useState<TypeUsers | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate('/login', { replace: true })
-  }
+    navigate('/login', { replace: true });
+  };
+
+  const fetchUsers = async () => {
+    try {
+      const url = 'http://localhost:300/user/search?keyword=%&page=2';
+      const headers = {
+        Authorization: '73ca199b-5173-42a5-b674-44cb2c6ab6c3',
+      };
+      const response = await axios.get<TypeUsers>(url, { headers });
+      setUsers(response.data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
-    <div className='flex flex-col gap-2 mt-40'>
+    <div className='flex flex-col gap-2 justify-between items-center text-center'>
+      {isLoading ? (
+        <p>Loading users...</p>
+      ) : (
+        <div className='prose'>
+          {users?.data.map((user) => (
+            <div className='flex flex-col justify-between items-center border-b-2' key={user.id}>
+              <img className='w-20 h-20' src={`${process.env.REACT_APP_BASE_URL}${user.profil.avatar}`}/>
+              <strong>{user.username}</strong>
+              <p>{user.profil.name}</p>
+              <i>{user.profil.address?.city} </i>
+              <b>{user.profil.address?.province}</b>
+            </div>
+          ))}
+        </div>
+      )}
       <Button onClick={handleClick} variant='primary'>Login</Button>
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
