a647f983009163d039dca3d437aa4270cde4d1ac
Yusuf Fadlulloh <fhaziefer99@gmail.com>
2024-04-09 08:07:53 +0700

    Updating Button Components

--- src/Components/Ui/Button.tsx
+++ src/Components/Ui/Button.tsx
@@ -2,12 +2,13 @@ import { cva } from 'class-variance-authority';
 import { cn } from '../../Utils/cn';
 
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
-    variant?: 'neutral' | 'primary' | 'secondary' | 'accent' | 'ghost' | 'link' ;
+    variant?: 'outline' | 'neutral' | 'primary' | 'secondary' | 'ghost' | 'link';
     disabled?: boolean
+    size?: 'normal' | 'tiny' | 'small' | 'large'
 }
 
-export default function Button({className, disabled, variant, ...props}: ButtonProps) {
-    return <button disabled={disabled} {...props} className={cn(buttonVariants({variant}), className)} />;
+export default function Button({ className, disabled, variant, size, ...props }: ButtonProps) {
+    return <button disabled={disabled} {...props} className={cn(buttonVariants({ variant, size }), className)} />;
 }
 
 const buttonVariants = cva(
@@ -15,16 +16,23 @@ const buttonVariants = cva(
     {
         variants: {
             variant: {
-                neutral: 'btn btn-neutral',
-                primary: 'btn btn-primary text-white hover:text-gray-100',
-                secondary: 'btn btn-secondary text-white hover:text-gray-100',
-                accent: 'btn btn-accent hover:text-gray-800',
-                ghost: 'btn btn-ghost hover:text-white',
-                link: 'btn btn-link',
+                outline: 'btn-outline hover:bg-gray-400 hover:text-black',
+                neutral: 'btn-neutral',
+                primary: 'btn-primary text-white hover:text-gray-100',
+                secondary: 'btn-secondary text-white hover:text-gray-100',
+                ghost: 'btn-ghost hover:text-white',
+                link: 'btn-link',
+            }, 
+            size: {
+                normal: '',
+                tiny: 'btn-xs',
+                small: 'btn-sm',
+                large: 'btn-lg',
             }
         },
         defaultVariants: {
-            variant: 'neutral'
+            variant: 'neutral',
+            size: 'normal'
         }
     }
 )
