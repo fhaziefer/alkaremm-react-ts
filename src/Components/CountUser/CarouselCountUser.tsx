1a11533cb2e006116391d8ac842ed8b01c209cdf
Yusuf Fadlulloh <fhaziefer99@gmail.com>
2024-04-26 14:20:40 +0700

    error handling

--- src/Components/CountUser/CarouselCountUser.tsx
+++ src/Components/CountUser/CarouselCountUser.tsx
@@ -30,6 +30,7 @@ const CarouselCountUser = ({ ...props }: Props) => {
             return data
         } else {
             setCountUser(data.data)
+            setError(false)
             setIsLoading(false)
         }
     }
@@ -44,98 +45,100 @@ const CarouselCountUser = ({ ...props }: Props) => {
             {isLoading ? <Loading /> :
                 <div className="flex flex-col items-center gap-1 my-8 bg-base-200 rounded-box py-4 border border-gray-500 shadow">
                     {props.label && <><h1 className="font-bold text-2xl text-center max-w-[80%]">{props.label}</h1> <h1 className="text-xs italic text-gray-600">(Terakhir diperbaharui: {dateNow})</h1></>}
-                    <div className="w-full carousel rounded-box bg-base-200">
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Bani Abdul Karim"
-                                number={countUser?.data.totalUser}
-                                sublabel="Orang"
-                            />
+                    {error ? <span className="text-error my-10 font-bold text-xl">Error, tidak dapat memuat data</span> :
+                        <div className="w-full carousel rounded-box bg-base-200">
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Bani Abdul Karim"
+                                    number={countUser?.data.totalUser}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Bani Hannah"
+                                    number={countUser?.data.totalHannah}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Bani Salamah"
+                                    number={countUser?.data.totalSalamah}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Bani Aisyah"
+                                    number={countUser?.data.totalAisyah}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Bani Maryam"
+                                    number={countUser?.data.totalMaryam}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Bani Zainab"
+                                    number={countUser?.data.totalZainab}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Bani Qomariyah"
+                                    number={countUser?.data.totalQomariyah}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Masih Hidup"
+                                    number={countUser?.data.totalAlive}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Sudah Wafat"
+                                    number={countUser?.data.totalDeath}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Laki-laki"
+                                    number={countUser?.data.totalMale}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Perempuan"
+                                    number={countUser?.data.totalFemale}
+                                    sublabel="Orang"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    number={countUser?.data.totalFamily}
+                                    sublabel="Keluarga"
+                                />
+                            </div>
+                            <div className="carousel-item">
+                                <CountUser
+                                    label="Belum Menikah"
+                                    number={countUser?.data.totalSingle}
+                                    sublabel="Orang"
+                                />
+                            </div>
                         </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Bani Hannah"
-                                number={countUser?.data.totalHannah}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Bani Salamah"
-                                number={countUser?.data.totalSalamah}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Bani Aisyah"
-                                number={countUser?.data.totalAisyah}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Bani Maryam"
-                                number={countUser?.data.totalMaryam}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Bani Zainab"
-                                number={countUser?.data.totalZainab}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Bani Qomariyah"
-                                number={countUser?.data.totalQomariyah}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Masih Hidup"
-                                number={countUser?.data.totalAlive}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Sudah Wafat"
-                                number={countUser?.data.totalDeath}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Laki-laki"
-                                number={countUser?.data.totalMale}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Perempuan"
-                                number={countUser?.data.totalFemale}
-                                sublabel="Orang"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                number={countUser?.data.totalFamily}
-                                sublabel="Keluarga"
-                            />
-                        </div>
-                        <div className="carousel-item">
-                            <CountUser
-                                label="Belum Menikah"
-                                number={countUser?.data.totalSingle}
-                                sublabel="Orang"
-                            />
-                        </div>
-                    </div>
+                    }
                 </div>
             }
         </>
